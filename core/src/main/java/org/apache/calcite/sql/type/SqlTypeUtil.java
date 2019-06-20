c2567d53905ceb4affaca24fd267563dab81d5ea
Temple Zhou <dbaztp@gmail.com>
2019-06-20 20:17:28 +0800

    KYLIN-4053 Compare date with timestamp

--- core/src/main/java/org/apache/calcite/sql/type/SqlTypeUtil.java
+++ core/src/main/java/org/apache/calcite/sql/type/SqlTypeUtil.java
@@ -1267,6 +1267,12 @@ public abstract class SqlTypeUtil {
         return true;
       }
     }
+    
+    // HACK POINT: implicitly convert date to timestamp
+    if ((family1 == SqlTypeFamily.DATE && family2 == SqlTypeFamily.TIMESTAMP)
+            || (family1 == SqlTypeFamily.TIMESTAMP && family2 == SqlTypeFamily.DATE)) {
+      return true;
+    }
 
     return false;
   }
