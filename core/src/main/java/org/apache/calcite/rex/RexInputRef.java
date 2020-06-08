575c7abe5b0abd1745409ec8dcc43091d27e9c80
xinbei <xinbei.fu.gr@dartmouth.edu>
2020-06-08 15:46:26 +0800

    KE-14714 make rexInputRef unique

--- core/src/main/java/org/apache/calcite/rex/RexInputRef.java
+++ core/src/main/java/org/apache/calcite/rex/RexInputRef.java
@@ -66,6 +66,10 @@ public class RexInputRef extends RexSlot {
     super(createName(index), index, type);
   }
 
+  public RexInputRef(String name, int index, RelDataType type) {
+    super(name, index, type);
+  }
+
   //~ Methods ----------------------------------------------------------------
 
   @Override public boolean equals(Object obj) {
@@ -85,11 +89,19 @@ public class RexInputRef extends RexSlot {
     return of(index, rowType.getFieldList());
   }
 
+  public static RexInputRef of(String name, int index, RelDataType rowType) {
+    return of(name, index, rowType.getFieldList());
+  }
+
   /**
    * Creates a reference to a given field in a list of fields.
    */
   public static RexInputRef of(int index, List<RelDataTypeField> fields) {
     return new RexInputRef(index, fields.get(index).getType());
+  }
+
+  public static RexInputRef of(String name, int index, List<RelDataTypeField> fields) {
+    return new RexInputRef(name, index, fields.get(index).getType());
   }
 
   /**
