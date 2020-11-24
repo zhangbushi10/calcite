1ddc6e413237a45e223f7fae5fe5829cb1faae26
lxian2shell <lxian2shell@gmail.com>
2020-11-24 14:47:27 +0800

    KE 20343 correct ssl verf (#181)
    see: https://github.com/apache/calcite/commit/43eeafcbac29d02c72bd520c003cdfc571de2d15

--- core/src/main/java/org/apache/calcite/runtime/HttpUtils.java
+++ core/src/main/java/org/apache/calcite/runtime/HttpUtils.java
@@ -23,13 +23,9 @@ import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSession;
 
 /**
  * Utilities for connecting to REST services such as Splunk via HTTP.
@@ -39,25 +35,7 @@ public class HttpUtils {
 
   public static HttpURLConnection getURLConnection(String url)
       throws IOException {
-    URLConnection conn = new URL(url).openConnection();
-    final HttpURLConnection httpConn = (HttpURLConnection) conn;
-
-    // take care of https stuff - most of the time it's only needed to
-    // secure client/server comm
-    // not to establish the identity of the server
-    if (httpConn instanceof HttpsURLConnection) {
-      HttpsURLConnection httpsConn = (HttpsURLConnection) httpConn;
-      httpsConn.setSSLSocketFactory(
-          TrustAllSslSocketFactory.createSSLSocketFactory());
-      httpsConn.setHostnameVerifier(
-          new HostnameVerifier() {
-            public boolean verify(String arg0, SSLSession arg1) {
-              return true;
-            }
-          });
-    }
-
-    return httpConn;
+    return (HttpURLConnection) new URL(url).openConnection();
   }
 
   public static void appendURLEncodedArgs(
